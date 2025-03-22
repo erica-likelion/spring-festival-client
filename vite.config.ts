a814ada93b89b0713d6a40106b11ac9c98b97418
SeHyun <rla6170@naver.com>
2025-03-23 00:33:13 +0900

    chore: vite-plugin-compression2 library - text compression feature

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import tsConfigPaths from 'vite-tsconfig-paths';
+import { compression } from 'vite-plugin-compression2';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,7 @@ export default defineConfig({
         },
       },
     }),
+    compression(),
     tsConfigPaths(),
     VitePWA({
       strategies: 'injectManifest',
