201cbbe2185c19ebe57539c36c9cd65403b0e129
SeHyun <rla6170@naver.com>
2025-04-08 16:47:55 +0900

    fix: alias 변경 반영

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from '@app/styles';
-import { theme } from '@app/styles';
+import { GlobalStyle } from '@/styles';
+import { theme } from '@/styles';
 
 createRoot(document.getElementById('root')!).render(
   <>
