19599844294f6e75be0714e680897d548933ea7f
SeHyun <rla6170@naver.com>
2025-04-08 16:24:32 +0900

    chore: 기존 project architecture 제거 및 변경

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -3,5 +3,5 @@ import { Theme } from './theme';
 
 declare module 'styled-components' {
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  export interface DefaultTheme extends Theme{}
+  export interface DefaultTheme extends Theme {}
 }
