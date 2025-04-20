c2d2af2e1630f6441171a6f9129f5a446bbd37ef
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-04-20 22:19:23 +0900

    revert: 메인페이지 복구

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,15 +1,8 @@
-import { DefaultNavBar, SearchNavBar } from '@/components/nav-bar/NavBar';
-
 export default function Main() {
-  const onclick = () => {};
-  const on = () => {};
   return (
     <>
-      <DefaultNavBar isBack={false} isSearch={true}></DefaultNavBar>
-      <DefaultNavBar isBack={true} title="page" isSearch={true}></DefaultNavBar>
-      <DefaultNavBar isBack={true} title="page" isSearch={false}></DefaultNavBar>
-      <DefaultNavBar isBack={false} isSearch={false}></DefaultNavBar>
-      <SearchNavBar onClick={onclick} placeholder="Text" onChange={on}></SearchNavBar>
+      {/**메인 페이지 */}
+      MAIN
     </>
   );
 }
