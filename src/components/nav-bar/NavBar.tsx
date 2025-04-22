07efc1ce98b10cfae5db28bcd139479c58f99337
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-04-23 03:32:41 +0900

    refactor: useNavigate 뒤로가기 로직 변경

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -4,26 +4,26 @@ import BackIcon from '@/assets/icons/nrk_chevron-left.svg?react';
 import LogoIcon from '@/assets/icons/Logo_Sample.svg?react';
 
 import { DefaultNavBarProps, SearchNavBarProps } from './NavBar.type';
+import { useNavigate } from 'react-router-dom';
 
 /**
- * NavBar 컴포넌트
  * DefaultNavBar 컴포넌트
  * @param {boolean} isBack - 뒤로가기 버튼 표시 여부
  * @param {boolean} isSearch - 검색 아이콘 표시 여부
  * @param {string} title - 제목 텍스트
+ * @returns {JSX.Element} DefaultNavBar 컴포넌트
+ * @example
  *
- * SearchNavBar 컴포넌트
- * @param {string} placeholder - 검색창의 placeholder 텍스트
- * @param {() => void} [onChange] - 검색창 입력 이벤트 핸들러
- * @param {() => void} [onClick] - Input 내 검색 아이콘 클릭 이벤트 핸들러
- * @returns {JSX.Element} NavBar 컴포넌트(Default, Search)
+ * <DefaultNavBar isBack={true} title="Page" isSearch={true}/>
+ * />
  */
 
-const handleBack = () => {
-  window.history.back(); // 뒤로가기
-};
-
 const DefaultNavBar: React.FC<DefaultNavBarProps> = ({ isBack, isSearch, title }) => {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1); // 뒤로가기
+  };
   return (
     <S.Container>
       {isBack ? (
@@ -38,7 +38,23 @@ const DefaultNavBar: React.FC<DefaultNavBarProps> = ({ isBack, isSearch, title }
   );
 };
 
+/**
+ * SearchNavBar 컴포넌트
+ * @param {string} placeholder - 검색창의 placeholder 텍스트
+ * @param {() => void} [onChange] - 검색창 입력 이벤트 핸들러
+ * @param {() => void} [onClick] - Input 내 검색 아이콘 클릭 이벤트 핸들러
+ * @returns {JSX.Element} SearchNavBar 컴포넌트
+ * @example
+    <SearchNavBar onClick={onClick} placeholder="Text" onChange={onChange} />
+ * 
+ */
 const SearchNavBar: React.FC<SearchNavBarProps> = ({ placeholder, onChange, onClick }) => {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1); // 뒤로가기
+  };
+
   return (
     <S.Container>
       <BackIcon width={24} height={24} onClick={handleBack} />
