6c8231d909a416e697a3e3ecaf39d2b02cf43565
ksundev <kshk08@hanyang.ac.kr>
2025-04-23 01:32:07 +0900

    refactor(notification): onClose 삭제 및 자체적 구현. 이미지 파일 경로 수정. 삭제 애니메이션 구현

--- src/components/notification/Notification.styles.ts
+++ src/components/notification/Notification.styles.ts
@@ -1,15 +1,34 @@
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 
-export const Container = styled.div`
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateY(-1.25rem);
+  }
+`;
+
+export const Container = styled.div<{ $isClosing?: boolean }>`
   display: flex;
   justify-content: space-between;
   align-items: center;
   gap: 0.5rem;
   min-width: 20rem;
-  width: 100%;
+  width: 20.9375rem;
+  height: 2.5rem;
   padding: 0.5rem 1rem;
   background-color: ${(props) => props.theme.colors.grayScale.gy900};
   border-radius: 0.5rem;
+  transition: all 0.3s ease;
+
+  ${(props) =>
+    props.$isClosing &&
+    css`
+      animation: ${fadeOut} 0.3s ease forwards;
+    `}
 `;
 
 export const Icon = styled.img`
@@ -27,7 +46,6 @@ export const Title = styled.p`
   white-space: nowrap;
   flex-grow: 1;
   min-width: 0;
-  max-width: 100%;
   margin: 0;
   padding: 0;
   text-align: left;
@@ -43,11 +61,4 @@ export const CloseButton = styled.button`
   cursor: pointer;
   height: 100%;
   margin: 0;
-
-  img {
-    width: 1.25rem;
-    height: 1.25rem;
-    flex-shrink: 0;
-    aspect-ratio: 1/1;
-  }
 `;
--- src/components/notification/Notification.tsx
+++ src/components/notification/Notification.tsx
@@ -1,40 +1,41 @@
+import { useState } from 'react';
 import * as S from './Notification.styles';
 import { NotificationProps } from './Notification.types';
-
+import CloseIcon from '@/assets/icons/close.svg?react';
 /**
  * 사용자에게 알림 메시지를 표시하는 Notification 컴포넌트입니다.
  * 클릭하면 확장되어 더 자세한 내용을 볼 수 있고, 닫기 버튼으로 제거할 수 있습니다.
  *
  * @param {string} title - 알림 제목
- * @param {function} onClose - 닫기 버튼 클릭 시 호출되는 콜백 함수
  * @param {function} onClick - 알림 클릭 시 호출되는 콜백 함수
  * @returns {React.ReactElement} Notification 컴포넌트
- * 
+ *
  * @example
  * // 기본 사용법
- * const [showNotification, setShowNotification] = useState(true);
- * const handleCloseNotification = () => {
-    setShowNotification(false);
-  };
  * <Notification
  *   title="Notification Title"
- *   contents="This is the content of the notification."
- *   onClose={handleCloseNotification}
- *   initExpanded={false}
- * /> 
+ *   onClick={handleClickNotification}
+ * />
  */
-export default function Notification({ title, onClose, onClick }: NotificationProps) {
+export default function Notification({ title, onClick }: NotificationProps) {
+  const [isVisible, setIsVisible] = useState(true);
+  const [isClosing, setIsClosing] = useState(false);
   const handleClose = (e: React.MouseEvent) => {
     e.stopPropagation();
-    onClose();
+    setIsClosing(true);
+    setTimeout(() => {
+      setIsVisible(false);
+    }, 300); // 3초 후에 사라지는 애니메이션 적용 (css와 일치)
   };
 
+  if (!isVisible) return null;
+
   return (
-    <S.Container>
-      <S.Icon src="src/assets/icons/notification.webp" alt="Notification" />
-      <S.Title onClick={onClick}>{title}</S.Title>
+    <S.Container $isClosing={isClosing}>
+      <S.Icon src="src/assets/images/notification.webp" alt="Notification" />
+      <S.Title onClick={() => onClick?.()}>{title}</S.Title>
       <S.CloseButton onClick={handleClose}>
-        <img src="src/assets/icons/close.svg" alt="Close" />
+        <CloseIcon width={'1.25rem'} height={'1.25rem'} />
       </S.CloseButton>
     </S.Container>
   );
