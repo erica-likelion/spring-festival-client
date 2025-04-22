5241e7a8ed340fc2d85f03a71df8e903269fe39e
ksundev <kshk08@hanyang.ac.kr>
2025-04-23 04:44:41 +0900

    feat(tabs): 선택적 파라미터 추가로 넓이 옵션 추가

--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -6,12 +6,13 @@ import { TabsProps } from './Tabs.types';
  * @param tabs tabs string array
  * @param activeTab active tab string
  * @param onTabClick click event handler
+ * @param autoWidth optional prop to set width to auto
  * @returns {JSX.Element}
  */
 
-export default function Tabs({ tabs, activeTab, onTabClick }: TabsProps) {
+export default function Tabs({ tabs, activeTab, onTabClick, autoWidth = false }: TabsProps) {
   return (
-    <S.TabsContainer>
+    <S.TabsContainer $autoWidth={autoWidth}>
       {tabs.map((tab) => (
         <S.Tab key={tab} isActive={tab === activeTab} onClick={() => onTabClick(tab)}>
           <S.TabText isActive={tab === activeTab}>{tab}</S.TabText>
