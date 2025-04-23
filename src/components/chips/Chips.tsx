0ded1cc3f4eef86d480f892baa2301fde82aa19b
javadocq <han18264@naver.com>
2025-04-23 16:40:21 +0900

    docs: jsdoc 추가

--- src/components/chips/Chips.tsx
+++ src/components/chips/Chips.tsx
@@ -2,6 +2,15 @@ import * as S from './Chips.styles';
 import CloseIcon from '@/assets/icons/close.svg?react';
 import { ChipsProps } from './Chips.types';
 
+/**
+ * Chips component
+ * @param chips chip string array
+ * @param onChipClick click event handler
+ * @param onChipClose click event handler
+ * @param autoWidth optional prop to set width to auto
+ * @returns {JSX.Element}
+ */
+
 export default function Chips({ chips, autoWidth = false, onChipClick, onChipClose }: ChipsProps) {
   return (
     <S.ChipsContainer $autoWidth={autoWidth}>
