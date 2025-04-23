979e2eddb5175c3cde4463909fcc556e12d4213f
javadocq <han18264@naver.com>
2025-04-23 16:33:41 +0900

    feat: chip 이동 , 삭제 로직 구현

--- src/components/chips/Chips.tsx
+++ src/components/chips/Chips.tsx
@@ -2,13 +2,20 @@ import * as S from './Chips.styles';
 import CloseIcon from '@/assets/icons/close.svg?react';
 import { ChipsProps } from './Chips.types';
 
-export default function Chips({ chips, autoWidth = false }: ChipsProps) {
+export default function Chips({ chips, autoWidth = false, onChipClick, onChipClose }: ChipsProps) {
   return (
     <S.ChipsContainer $autoWidth={autoWidth}>
       {chips.map((chip) => (
-        <S.Chip key={chip}>
+        <S.Chip key={chip} onClick={() => onChipClick?.(chip)}>
           <S.ChipText>{chip}</S.ChipText>
-          <CloseIcon width={'0.75rem'} height={'0.75rem'} />
+          <CloseIcon
+            width={'0.75rem'}
+            height={'0.75rem'}
+            onClick={(e) => {
+              e.stopPropagation();
+              onChipClose?.(chip);
+            }}
+          />
         </S.Chip>
       ))}
     </S.ChipsContainer>
