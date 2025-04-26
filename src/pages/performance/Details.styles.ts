0ccb50491cae7e2e05c37e0c8a98073b6712a07d
javadocq <han18264@naver.com>
2025-04-27 02:23:07 +0900

    feat: 대표곡 추가

--- src/pages/performance/Details.styles.ts
+++ src/pages/performance/Details.styles.ts
@@ -3,11 +3,62 @@ import styled from 'styled-components';
 export const DetailsContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Image = styled.img`
   width: 23.4888rem;
   height: 29.375rem;
   aspect-ratio: 375.82/470;
-  background: url("<path-to-image>") lightgray 0px 0px / 100% 100% no-repeat;
+  background: url('<path-to-image>') lightgray 0px 0px / 100% 100% no-repeat;
+`;
+
+export const TitleSongWrap = styled.div`
+  display: flex;
+  width: 20.7011rem;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1rem;
+`;
+
+export const TitleSongText = styled.p`
+  align-self: stretch;
+  ${(props) => props.theme.fonts.header.h3};
+  color: ${(props) => props.theme.colors.grayScale.white};
+`;
+
+export const SongWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.375rem;
+  align-self: stretch;
+`;
+
+export const SongBox = styled.div`
+  display: flex;
+  padding: 0.5rem 0rem;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+`;
+
+export const Wrap = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.875rem;
+`;
+
+export const SongImage = styled.div`
+  width: 3rem;
+  height: 3rem;
+  aspect-ratio: 1/1;
+  border-radius: 0.5rem;
+  background: #c4c4c4;
+`;
+
+export const SongName = styled.p`
+  ${(props) => props.theme.fonts.header.h4};
+  color: ${(props) => props.theme.colors.grayScale.white};
 `;
--- src/pages/performance/Details.tsx
+++ src/pages/performance/Details.tsx
@@ -1,11 +1,27 @@
 import TestImage from '@/assets/images/performance/day1-newjins.webp';
+import SongIcon from '@/assets/icons/song.svg?react';
 import * as S from './Details.styles';
 
 export default function Details() {
+  const songList = ['Hurt', 'Love is Gone', 'Love is Gone (Acoustic)'];
   return (
     <S.DetailsContainer>
       {/* Navbar back version */}
       <S.Image src={TestImage} alt="newjins" width={'23.4888rem'} height={'29.375rem'} />
+      <S.TitleSongWrap>
+        <S.TitleSongText>대표곡</S.TitleSongText>
+        <S.SongWrap>
+          {songList.map((song, index) => (
+            <S.SongBox key={index}>
+              <S.Wrap>
+                <S.SongImage />
+                <S.SongName key={index}>{song}</S.SongName>
+              </S.Wrap>
+              <SongIcon width={'1.5rem'} height={'1.5rem'} />
+            </S.SongBox>
+          ))}
+        </S.SongWrap>
+      </S.TitleSongWrap>
     </S.DetailsContainer>
   );
 }
