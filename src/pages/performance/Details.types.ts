aadbe22b3c19bb297bff94481928573e715c1340
javadocq <han18264@naver.com>
2025-04-27 18:45:26 +0900

    feat: type 추가

---
+++ src/pages/performance/Details.types.ts
@@ -0,0 +1,7 @@
+export type PerformanceDetailsProps = {
+  backgroundUrl: string; // 배경 이미지 URL
+  singer: string; // 가수 이름
+  time: string; // 공연 시간 (ex: "21:00~22:00")
+  description: string; // 가수 설명
+  songList: string[]; // 대표곡 리스트
+};
