37d4eb38bd0e17601625169d9d32d170ab920e2b
javadocq <han18264@naver.com>
2025-04-29 21:38:23 +0900

    feat: 캐러셀 제외 전반적인 요소 추가

--- src/pages/performance/Performance.styles.ts
+++ src/pages/performance/Performance.styles.ts
@@ -3,6 +3,110 @@ import styled from 'styled-components';
 export const PerformanceContainer = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+`;
+
+export const InfoWrap = styled.div`
+  display: flex;
+  min-width: 20.9375rem;
+  flex-direction: column;
+  align-items: flex-start;
   gap: 1rem;
-  padding: 1rem;
+  margin-top: 1rem;
+`;
+
+export const TodayPerformanceText = styled.p`
+  align-self: stretch;
+  ${(props) => props.theme.fonts.header.h3};
+  color: ${(props) => props.theme.colors.grayScale.white};
+`;
+
+export const DayWrap = styled.div`
+  display: flex;
+  min-width: 20.9375rem;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.375rem;
+  margin-top: 1.5rem;
+`;
+
+export const TextWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+`;
+
+export const StartText = styled.p`
+  ${(props) => props.theme.fonts.body.xsmall500};
+  color: ${(props) => props.theme.colors.grayScale.gy400};
+`;
+
+export const Carousel = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.75rem;
+  align-self: stretch;
+  margin-top: 1rem;
+`;
+
+export const SingerTimeWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.375rem;
+`;
+
+export const SingerName = styled.p`
+  ${(props) => props.theme.fonts.header.h1};
+  color: ${(props) => props.theme.colors.grayScale.white};
+`;
+
+export const TimeBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 0.375rem;
+`;
+
+export const TimeText = styled.p`
+  ${(props) => props.theme.fonts.body.xsmall500};
+  color: ${(props) => props.theme.colors.grayScale.gy300};
+`;
+
+export const AlertBox = styled.div`
+  display: flex;
+  padding: 0.25rem 0.75rem 0.25rem 0.625rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.375rem;
+`;
+
+export const AlertText = styled.p`
+  ${(props) => props.theme.fonts.body.xsmall500};
+  color: ${(props) => props.theme.colors.grayScale.white};
+  text-align: center;
+`;
+
+export const TableNoteWrap = styled.div`
+  display: flex;
+  min-width: 20.9375rem;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1rem;
+  margin-top: 2.5rem;
+`;
+
+export const FrameBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.44rem;
+`;
+
+export const NoteText = styled.p`
+  align-self: stretch;
+  ${(props) => props.theme.fonts.header.h3};
+  color: ${(props) => props.theme.colors.grayScale.white};
 `;
--- src/pages/performance/Performance.tsx
+++ src/pages/performance/Performance.tsx
@@ -1,59 +1,68 @@
-import ImageTextIconFrame from '@/components/image-text-icon-frame/ImageTextIconFrame';
 import * as S from './Performance.styles';
-import { NavBar } from '@/components/nav-bar/NavBar';
-import Notification from '@/components/notification/Notification';
-import Tabs from '@/components/tabs/Tabs';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { NavBar } from '@/components/nav-bar';
+import { Notification } from '@/components/notification';
+import { Tabs } from '@/components/tabs';
+import { ImageTextIconFrame } from '@/components/image-text-icon-frame';
+import HelpIcon from '@/assets/icons/help_gy600.svg?react';
+import TimeIcon from '@/assets/icons/time_gy200.svg?react';
+import AlertIcon from '@/assets/icons/alert.svg?react';
 
 export default function Performance() {
   const navigate = useNavigate();
   const [selectedDay, setSelectedDay] = useState<string>('1일차');
+
   return (
     <S.PerformanceContainer>
       <NavBar isBack={false} />
-      <div>
-        <p>오늘의 공연</p>
+      <S.InfoWrap>
+        <S.TodayPerformanceText>오늘의 공연</S.TodayPerformanceText>
         <Notification title="미성년자 입장 제한 안내" />
-      </div>
-      <div>
-        <p>공연 시작 17:00시</p>
-        {/* 아이콘 */}
-      </div>
-      <Tabs
-        tabs={['1일차', '2일차', '3일차']}
-        activeTab={selectedDay}
-        onTabClick={setSelectedDay}
-      />
-      <div>
+      </S.InfoWrap>
+      <S.DayWrap>
+        <S.TextWrap>
+          <S.StartText>공연 시작 17:00시</S.StartText>
+          <HelpIcon width={'1.125rem'} height={'1.125rem'} />
+        </S.TextWrap>
+        <Tabs
+          tabs={['1일차', '2일차', '3일차']}
+          activeTab={selectedDay}
+          onTabClick={setSelectedDay}
+        />
+      </S.DayWrap>
+
+      <S.Carousel>
         {/* 캐러셀 */}
-        <div>
-          <p>뉴진스</p>
-          <div>
-            {/* 시계 */}
-            <p>21:00~22:00</p>
-          </div>
-          <div>
-            {/* 종 */}
-            <p>알림 받기</p>
-          </div>
-        </div>
-      </div>
-      <div>
-        <p>공연 유의사항</p>
-        <ImageTextIconFrame
-          image=""
-          title="전체 타임 테이블 확인하기"
-          description="공연 일정 한 번에 확인하기"
-          onClick={() => navigate('/performance/timetable')}
-        />
-        <ImageTextIconFrame
-          image=""
-          title="❗️공연 유의사항 보러가기"
-          description="공연 보기 전 필독!"
-          onClick={() => navigate('/main/notice/details')}
-        />
-      </div>
+        <S.SingerTimeWrap>
+          <S.SingerName>뉴진스</S.SingerName>
+          <S.TimeBox>
+            <TimeIcon width={'1.125rem'} height={'1.125rem'} />
+            <S.TimeText>21:00~22:00</S.TimeText>
+          </S.TimeBox>
+          <S.AlertBox>
+            <AlertIcon width={'1rem'} height={'1rem'} />
+            <S.AlertText>알림 받기</S.AlertText>
+          </S.AlertBox>
+        </S.SingerTimeWrap>
+      </S.Carousel>
+      <S.TableNoteWrap>
+        <S.NoteText>공연 유의사항</S.NoteText>
+        <S.FrameBox>
+          <ImageTextIconFrame
+            image=""
+            title="전체 타임 테이블 확인하기"
+            description="공연 일정 한 번에 확인하기"
+            onClick={() => navigate('/performance/timetable')}
+          />
+          <ImageTextIconFrame
+            image=""
+            title="❗️공연 유의사항 보러가기"
+            description="공연 보기 전 필독!"
+            onClick={() => navigate('/main/notice/detail')}
+          />
+        </S.FrameBox>
+      </S.TableNoteWrap>
     </S.PerformanceContainer>
   );
 }
