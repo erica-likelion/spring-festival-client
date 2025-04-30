ea102b0a39ebb825d390c0eedf1debd6a59dc29c
javadocq <han18264@naver.com>
2025-05-01 01:31:28 +0900

    feat: timetable 컴포넌트 및 스타일 파일 추가 & 애니메이션 설정

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,7 @@
 import Main from '@/pages/Main';
 import Map from '@/pages/Map';
 import Login from '@/pages/Login';
-import Performance from '@/pages/Performance';
+import Performance from '@/pages/performance/Performance';
 import Booth from '@/pages/booth/Booth';
 import User from '@/pages/User';
 
