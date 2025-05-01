921dd1138c658122768a5209a3494fad99d7a6e5
javadocq <han18264@naver.com>
2025-05-01 19:54:26 +0900

    fix: songList에 url 추가

--- src/constants/performance/SingerInfo.ts
+++ src/constants/performance/SingerInfo.ts
@@ -10,11 +10,11 @@ export const performanceData = {
       description:
         'NewJeans는 대한민국의 걸그룹이다. 민지, 하니, 다니엘, 해린, 혜인 5인조로 구성되어 있다. 민희진이 프로듀싱을 맡았으며, 이들은 1990년대와 2000년대의 복고풍 레트로 음악 스타일을 현대적 시대에 맞게 재구성한 그룹이다. ',
       songList: [
-        { image: TestImage, name: 'OMG' },
-        { image: TestImage, name: 'Hype' },
-        { image: TestImage, name: 'Ditto' },
-        { image: TestImage, name: 'Attention' },
-        { image: TestImage, name: 'Cookie' },
+        { image: TestImage, name: 'OMG', url: 'https://www.youtube.com/watch?v=hc32lb0po9U' },
+        { image: TestImage, name: 'Hype', url: 'https://www.youtube.com/watch?v=hc32lb0po9U' },
+        { image: TestImage, name: 'Ditto', url: 'https://www.youtube.com/watch?v=hc32lb0po9U' },
+        { image: TestImage, name: 'Attention', url: 'https://www.youtube.com/watch?v=hc32lb0po9U' },
+        { image: TestImage, name: 'Cookie', url: 'https://www.youtube.com/watch?v=hc32lb0po9U' },
       ],
     },
     {
