ce88c9c7ab9a8aef6657a34d72d6c4ce2bbfa5f8
SeHyun <rla6170@naver.com>
2025-05-02 11:14:22 +0900

    refactor: waiting ref로 모달 포탈 열기로 변경

--- src/features/waiting/components/Waiting.tsx
+++ src/features/waiting/components/Waiting.tsx
@@ -3,11 +3,13 @@ import * as S from './Waiting.styles';
 import RightIcon from '@/assets/icons/right-arrow.svg?react';
 import useModal from '@/hooks/useModal';
 import WaitingModal from '@/features/waiting/components/WaitingModal';
+import { useRef } from 'react';
 
 export default function Waiting() {
   const { open } = useModal(WaitingModal);
+  const waitingRef = useRef<HTMLDivElement>(null);
   return (
-    <S.Container>
+    <S.Container ref={waitingRef}>
       <S.HeaderText>대기 현황</S.HeaderText>
       <S.HorizontalLine />
       <S.TextSection>
@@ -20,7 +22,10 @@ export default function Waiting() {
         <S.SmallText>웨이팅 가능 시간 : 금일 00:00 ~ 00:00</S.SmallText>
       </S.TextSection>
       <S.ButtonFrame>
-        <S.Button whileTap={{ scale: 0.9 }} onClick={() => open({ title: '웨이팅하기' })}>
+        <S.Button
+          whileTap={{ scale: 0.9 }}
+          onClick={() => open({ title: '웨이팅하기' }, { portalTarget: waitingRef.current })}
+        >
           <S.MediumText>웨이팅하기</S.MediumText>
           <S.MediumText>0 / 3</S.MediumText>
           <RightIcon width={'1.5rem'} height={'1.5rem'} />
