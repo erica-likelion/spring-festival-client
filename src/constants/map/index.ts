b05353dbab1068fa12af2b7ae869820a0e04552c
ksundev <kshk08@hanyang.ac.kr>
2025-05-03 00:52:27 +0900

    feat(map): 일자(days) 및 카테고리 상수 추가

---
+++ src/constants/map/index.ts
@@ -0,0 +1,7 @@
+import { days } from '@/constants/map/Days';
+import { categories } from '@/constants/map/Categories';
+import { DAYS } from '@/constants/map/Days';
+import { CATEGORIES } from '@/constants/map/Categories';
+
+export { days, categories };
+export type { DAYS, CATEGORIES };
