8ff4ef236c05d65b06545cff4585ba2c61a4b28e
SeHyun <rla6170@naver.com>
2025-05-02 11:33:36 +0900

    fix: modal 사용으로 인한 back 클릭 시 미반영 문제 수정

--- src/pages/booth/BoothDetail.tsx
+++ src/pages/booth/BoothDetail.tsx
@@ -1,11 +1,15 @@
+import { useRef } from 'react';
+import { useParams, useLocation } from 'react-router-dom';
 import { NavBar } from '@/components/nav-bar';
-import { useParams } from 'react-router-dom';
 import * as S from './BoothDetail.styles';
 import { BoothInfo, BoothLocation, MenuList } from '@/features/booth';
 import { Waiting } from '@/features/waiting';
+
 export default function BoothDetail() {
   const { id } = useParams();
-
+  const location = useLocation();
+  const fromRef = useRef(location.state?.from || '/booth');
+  console.log(fromRef.current);
   return (
     <>
       <NavBar isBack title="주점" />
