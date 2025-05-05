4ef2d991f1839dff4b9e436b3a4e36d5f41f225e
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-05-06 01:09:16 +0900

    feat: 글자 최대 55자까지지

--- src/features/main/notice/NoticeText.tsx
+++ src/features/main/notice/NoticeText.tsx
@@ -1,19 +1,34 @@
 import * as S from './NoticeText.styles';
 
 interface NoticeTextProps {
-  image: string;
+  image?: string;
   title: string;
   body: string;
 }
+/**
+ * 텍스트 55자 까지만 허용
+ * @param {string} text - 원본 텍스트
+ * @param {number} maxLength - 최대 표시할 글자 수
+ * @returns {string}
+ */
+
+const truncateText = (text: string, maxLength: number) => {
+  if (text.length > maxLength) {
+    return text.slice(0, maxLength) + '...';
+  }
+  return text;
+};
 
 export default function NoticeText({ image, title, body }: NoticeTextProps) {
+  const truncatedBody = truncateText(body, 55);
+
   return (
     <>
       <S.Container>
         <S.Image src={image} alt="notice" />
         <S.TextWrapper>
           <S.Title>{title}</S.Title>
-          <S.body>{body}</S.body>
+          <S.body>{truncatedBody}</S.body>
         </S.TextWrapper>
       </S.Container>
     </>
