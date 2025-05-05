f16daedf121f7582bf662981a0be4fda38e4a01a
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-05-05 15:21:56 +0900

    style: 캐러셀 레이아웃 잘림 방지

--- src/pages/main/Main.styles.ts
+++ src/pages/main/Main.styles.ts
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
 export const Main = styled.main`
-  margin: 0.88rem 1.25rem;
   overflow: hidden;
 `;
+
 export const Title = styled.div`
   ${(props) => props.theme.fonts.header.h3}
   color: ${(props) => props.theme.colors.grayScale.white};
+  margin: 0.88rem 1.25rem 0;
 `;
 
 export const CarouselsBox = styled.div`
