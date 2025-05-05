b9a38ba949a8cc0729affd316c6c6224ce1aa6b2
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-05-06 01:22:27 +0900

    feat: noticedata 연결

--- src/pages/main/Notice.styles.ts
+++ src/pages/main/Notice.styles.ts
@@ -4,4 +4,6 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 1rem;
+  flex-direction: column;
+  align-items: center;
 `;
--- src/pages/main/Notice.tsx
+++ src/pages/main/Notice.tsx
@@ -1,17 +1,21 @@
 import { NavBar } from '@/components/nav-bar';
 import * as S from './Notice.styles';
 import NoticeText from '@/features/main/notice/NoticeText';
+import { NoticeData } from '@/constants/main/Notice';
 
 export default function Notice() {
   return (
     <>
       <NavBar title="공지사항" isBack={true} />
       <S.Container>
-        <NoticeText
-          image=""
-          title="sdf"
-          body="Body text Body text Body text Body text Body text Body text Body text Body text Body text Body text"
-        />
+        {NoticeData.map((notice) => (
+          <NoticeText
+            key={'notice${notice.id}'}
+            image={notice.img}
+            title={notice.title}
+            body={notice.body}
+          />
+        ))}
       </S.Container>
     </>
   );
