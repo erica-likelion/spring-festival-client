b30e65aabeb3c0214c722bd31015031c94ad498c
kkeunii <greenlee55@naver.com>
2025-05-07 23:44:19 +0900

    feat: 모달 컴포넌트 적용 중

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,8 +1,11 @@
+import LoginButton from '../login/LoginModalPage';
+
 export default function Main() {
   return (
     <>
       {/**메인 페이지 */}
       MAIN
+      <LoginButton />
     </>
   );
 }
