fd93e82de5c6aa046609198d62b84e5230e32345
kkeunii <greenlee55@naver.com>
2025-05-08 19:02:13 +0900

    refactor: 로그인 모달 적용

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,11 +1,8 @@
-import LoginButton from '../login/LoginModalPage';
-
 export default function Main() {
   return (
     <>
       {/**메인 페이지 */}
       MAIN
-      <LoginButton />
     </>
   );
 }
