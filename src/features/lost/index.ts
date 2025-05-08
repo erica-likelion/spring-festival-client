bec2f76ae293e3488a43959febac7676e2e38c7f
javadocq <han18264@naver.com>
2025-05-08 15:02:53 +0900

    feat: title 컴포넌트 제작

--- src/features/lost/index.ts
+++ src/features/lost/index.ts
@@ -4,3 +4,4 @@ export { default as ItemCard } from './components/main/ItemCard';
 export { default as ModalNotification } from './modal/ModalNotification';
 export { default as ModalPost } from './modal/ModalPost';
 export { default as StaffLabel } from './components/main/StaffLabel';
+export { default as Title } from './components/upload/Title';
