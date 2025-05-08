eaf77c86ca4507544ccc169d2ca9336f55f0bf25
javadocq <han18264@naver.com>
2025-05-08 19:57:06 +0900

    feat: 이미지 업로드 로직 구현

--- src/features/lost/components/upload/ImageSection.styles.ts
+++ src/features/lost/components/upload/ImageSection.styles.ts
@@ -8,6 +8,43 @@ export const ImageBox = styled.div`
   align-self: stretch;
 `;
 
+export const Wrap = styled.div`
+  display: flex;
+  gap: 1rem;
+  align-self: stretch;
+  align-items: flex-start;
+`;
+
+export const PreviewImage = styled.img`
+  width: 6.625rem;
+  height: 8.6875rem;
+  border-radius: 0.75rem;
+  object-fit: cover;
+  background-color: lightgray;
+`;
+
+export const MariginBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 1rem;
+`;
+
+export const ReUploadButton = styled.button`
+  display: flex;
+  width: 7.5rem;
+  padding: 0.25rem 1rem;
+  margin-top: 1.37rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+  border-radius: 0.5rem;
+  border: none;
+  background-color: ${(props) => props.theme.colors.primary.bl400};
+  ${(props) => props.theme.fonts.body.small500};
+  color: ${(props) => props.theme.colors.grayScale.white};
+  cursor: pointer;
+`;
+
 export const ImageButton = styled.button`
   display: flex;
   width: 100%;
@@ -20,6 +57,7 @@ export const ImageButton = styled.button`
   border-radius: 0.75rem;
   border: 1px dashed ${(props) => props.theme.colors.grayScale.gy600};
   background-color: ${(props) => props.theme.colors.grayScale.black};
+  cursor: pointer;
 `;
 
 export const ImageButtonText = styled.p`
--- src/features/lost/components/upload/ImageSection.tsx
+++ src/features/lost/components/upload/ImageSection.tsx
@@ -4,35 +4,67 @@ import * as S from './ImageSection.styles';
 import Title from './Title';
 import CameraIcon from '@/assets/icons/nrk_camera.svg?react';
 import { ImageSectionProps } from './ImageSection.types';
+import { useRef } from 'react';
 
 export default function ImageSection({ image, setImage }: ImageSectionProps) {
+  const fileInputRef = useRef<HTMLInputElement>(null);
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) setImage(file);
   };
 
+  const handleReUpload = () => {
+    fileInputRef.current?.click();
+  };
+
+  const imageURL = image ? URL.createObjectURL(image) : null;
+
   return (
     <S.ImageBox>
       <Title title="분실물 사진" />
-      <label htmlFor="image-upload" style={{ display: 'block', width: '100%' }}>
-        <S.ImageButton as="div">
-          <CameraIcon width="1.5rem" height="1.5rem" fill={theme.colors.grayScale.gy50} />
-          <S.ImageButtonText>{image ? image.name : '이미지 업로드 (1장)'}</S.ImageButtonText>
-        </S.ImageButton>
-        <input
-          id="image-upload"
-          type="file"
-          accept="image/*"
-          onChange={handleImageChange}
-          style={{ display: 'none' }}
-        />
-      </label>
-      <S.CommentBox>
-        <Description
-          text={`* 잘 알아볼 수 있도록 밝은 곳에서 전체 모습이 나오게 촬영한 \n이미지를 올려주세요.`}
-        />
-        <Description text="* 이미지에 민감한 개인정보가 포함되어 있다면 가려서 업로드해주세요!" />
-      </S.CommentBox>
+      {imageURL ? (
+        <S.Wrap>
+          <S.PreviewImage src={imageURL} alt="업로드한 이미지" />
+          <S.CommentBox>
+            <Description
+              text={`* 잘 알아볼 수 있도록 밝은 곳에서 전체\n 모습이 나오게 촬영한 이미지를 올려주세요.`}
+            />
+            <Description text="* 이미지에 민감한 개인정보가 포함되어 있다면 가려서 업로드해주세요!" />
+            <S.ReUploadButton type="button" onClick={handleReUpload}>
+              다시 업로드하기
+            </S.ReUploadButton>
+          </S.CommentBox>
+        </S.Wrap>
+      ) : (
+        <>
+          <S.ImageButton as="div">
+            <CameraIcon width="1.5rem" height="1.5rem" fill={theme.colors.grayScale.gy50} />
+            <S.ImageButtonText>이미지 업로드 (1장)</S.ImageButtonText>
+          </S.ImageButton>
+          <S.MariginBox>
+            <S.CommentBox>
+              <Description
+                text={`* 잘 알아볼 수 있도록 밝은 곳에서 전체 모습이 나오게 촬영한 \n이미지를 올려주세요.`}
+              />
+              <Description text="* 이미지에 민감한 개인정보가 포함되어 있다면 가려서 업로드해주세요!" />
+            </S.CommentBox>
+          </S.MariginBox>
+        </>
+      )}
+      <input
+        id="image-upload"
+        type="file"
+        accept="image/*"
+        ref={fileInputRef}
+        onChange={handleImageChange}
+        style={{
+          position: 'absolute',
+          width: 0,
+          height: 0,
+          overflow: 'hidden',
+          opacity: 0,
+        }}
+      />
     </S.ImageBox>
   );
 }
