5b6d1d2d734824f55acb0ac95301163f998c7f77
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-05-11 01:50:40 +0900

    fix: 링크 폴더 분리 및 새창열기

--- src/features/main/components/menu/Menu.tsx
+++ src/features/main/components/menu/Menu.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import * as S from './Menu.styles';
 import { ImageTextIconFrame } from '@/components/image-text-icon-frame';
-import { useMemo } from 'react';
+import { externalLinks } from '@/constants/main/ExternalLink';
 
 /**
  * Menu 컴포넌트
@@ -13,17 +13,6 @@ import { useMemo } from 'react';
 export default function Menu() {
   const navigate = useNavigate();
 
-  const externalLinks = useMemo(
-    () => ({
-      instagramStudent: 'https://www.instagram.com/hanyang_erica_stu/',
-      instagramLikelion: 'https://www.instagram.com/likelion_erica/',
-      googleForm: 'https://forms.gle/2YmD7kKm1YJSmQSF7',
-      linkTree:
-        'https://linktr.ee/hanyangericastu?fbclid=PAZXh0bgNhZW0CMTEAAadthHc2bzHYTJpR9bKdWqWf5dkrhAQ4keGj3co0OaEQFbvxfv_pwv7r_6pQLw_aem_cSOQAKzd9acl-2z3bVSdmQ',
-    }),
-    [],
-  );
-
   return (
     <S.SectionContainer>
       <S.Section>
@@ -33,13 +22,13 @@ export default function Menu() {
             image="./"
             title="총학생회 인스타그램 바로가기"
             description="축제 공지를 한눈에 보아요"
-            onClick={() => (window.location.href = externalLinks.instagramStudent)}
+            onClick={() => window.open(externalLinks.instagramStudent, '_blank')}
           />
           <ImageTextIconFrame
             image="./"
             title="멋사 보러가볼까"
             description="멋사 인스타그램 바로가기"
-            onClick={() => (window.location.href = externalLinks.instagramLikelion)}
+            onClick={() => window.open(externalLinks.instagramLikelion, '_blank')}
           />
         </S.Frame>
       </S.Section>
@@ -55,19 +44,20 @@ export default function Menu() {
         </S.Frame>
       </S.Section>
       <S.Section>
+        {/* 링크 수정필요 */}
         <S.SubTitle>문의사항</S.SubTitle>
         <S.Frame>
           <ImageTextIconFrame
             image="./"
             title="축제 문의사항 남기기"
             description="축기단 구글폼으로 이동"
-            onClick={() => (window.location.href = externalLinks.linkTree)}
+            onClick={() => window.open(externalLinks.linkTree, '_blank')}
           />
           <ImageTextIconFrame
             image="./"
             title="축제 FAQ"
             description="총학생회 인스타그램"
-            onClick={() => (window.location.href = externalLinks.linkTree)}
+            onClick={() => window.open(externalLinks.linkTree, '_blank')}
           />
         </S.Frame>
       </S.Section>
@@ -77,7 +67,7 @@ export default function Menu() {
           image="./"
           title="축제 웹앱 사용후기"
           description="구글폼 링크"
-          onClick={() => (window.location.href = externalLinks.googleForm)}
+          onClick={() => window.open(externalLinks.googleForm, '_blank')}
         />
       </S.Section>
     </S.SectionContainer>
