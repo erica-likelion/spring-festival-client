21c5cfe0162c4c8c4bffc3f693eb852302f92b3a
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-05-10 17:22:55 +0900

    refactor: url캐싱

--- src/features/main/components/menu/Menu.tsx
+++ src/features/main/components/menu/Menu.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import * as S from './Menu.styles';
 import { ImageTextIconFrame } from '@/components/image-text-icon-frame';
+import { useMemo } from 'react';
 
 /**
  * Menu 컴포넌트
@@ -11,6 +12,18 @@ import { ImageTextIconFrame } from '@/components/image-text-icon-frame';
 
 export default function Menu() {
   const navigate = useNavigate();
+
+  const externalLinks = useMemo(
+    () => ({
+      instagramStudent: 'https://www.instagram.com/hanyang_erica_stu/',
+      instagramLikelion: 'https://www.instagram.com/likelion_erica/',
+      googleForm: 'https://forms.gle/2YmD7kKm1YJSmQSF7',
+      linkTree:
+        'https://linktr.ee/hanyangericastu?fbclid=PAZXh0bgNhZW0CMTEAAadthHc2bzHYTJpR9bKdWqWf5dkrhAQ4keGj3co0OaEQFbvxfv_pwv7r_6pQLw_aem_cSOQAKzd9acl-2z3bVSdmQ',
+    }),
+    [],
+  );
+
   return (
     <S.SectionContainer>
       <S.Section>
@@ -20,13 +33,13 @@ export default function Menu() {
             image="./"
             title="총학생회 인스타그램 바로가기"
             description="축제 공지를 한눈에 보아요"
-            onClick={() => (window.location.href = 'https://www.instagram.com/hanyang_erica_stu/')}
+            onClick={() => (window.location.href = externalLinks.instagramStudent)}
           />
           <ImageTextIconFrame
             image="./"
             title="멋사 보러가볼까"
             description="멋사 인스타그램 바로가기"
-            onClick={() => (window.location.href = 'https://www.instagram.com/likelion_erica/')}
+            onClick={() => (window.location.href = externalLinks.instagramLikelion)}
           />
         </S.Frame>
       </S.Section>
@@ -48,19 +61,13 @@ export default function Menu() {
             image="./"
             title="축제 문의사항 남기기"
             description="축기단 구글폼으로 이동"
-            onClick={() =>
-              (window.location.href =
-                'https://linktr.ee/hanyangericastu?fbclid=PAZXh0bgNhZW0CMTEAAadthHc2bzHYTJpR9bKdWqWf5dkrhAQ4keGj3co0OaEQFbvxfv_pwv7r_6pQLw_aem_cSOQAKzd9acl-2z3bVSdmQ')
-            }
+            onClick={() => (window.location.href = externalLinks.linkTree)}
           />
           <ImageTextIconFrame
             image="./"
             title="축제 FAQ"
             description="총학생회 인스타그램"
-            onClick={() =>
-              (window.location.href =
-                'https://linktr.ee/hanyangericastu?fbclid=PAZXh0bgNhZW0CMTEAAadthHc2bzHYTJpR9bKdWqWf5dkrhAQ4keGj3co0OaEQFbvxfv_pwv7r_6pQLw_aem_cSOQAKzd9acl-2z3bVSdmQ')
-            }
+            onClick={() => (window.location.href = externalLinks.linkTree)}
           />
         </S.Frame>
       </S.Section>
@@ -70,7 +77,7 @@ export default function Menu() {
           image="./"
           title="축제 웹앱 사용후기"
           description="구글폼 링크"
-          onClick={() => (window.location.href = 'https://forms.gle/2YmD7kKm1YJSmQSF7')}
+          onClick={() => (window.location.href = externalLinks.googleForm)}
         />
       </S.Section>
     </S.SectionContainer>
