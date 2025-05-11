5971a7e4a02d273ab48cfb021a61cd8ddd289aa2
ksundev <kshk08@hanyang.ac.kr>
2025-05-12 07:51:26 +0900

    feat: 바텀 시트 관련 상수 추가

---
+++ src/constants/map/BottomSheetOptions.ts
@@ -0,0 +1,3 @@
+export const MIN_Y = 124; // 바텀 시트가 최대로 올라 갔을 때의 Y좌표 값 (화면 최상단에서 떨어진 정도)
+export const MAX_Y = window.innerHeight - 260; //바텀 시트가 최대로 내려갔을 때의 Y좌표 값
+export const BOTTOM_SHEET_HEIGHT = '100vh'; //바텀시트 높이 - 전체 화면 높이로 설정
