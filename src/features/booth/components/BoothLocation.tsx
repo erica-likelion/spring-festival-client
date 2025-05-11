3ab6f6302207bfa8f3f8dc7f1e4736e344e959b1
SeHyun <rla6170@naver.com>
2025-05-11 16:56:45 +0900

    fix: location button right arrow

--- src/features/booth/components/BoothLocation.tsx
+++ src/features/booth/components/BoothLocation.tsx
@@ -1,4 +1,5 @@
 import * as S from './BoothLocation.styles';
+import RightIcon from '@/assets/icons/right-arrow.svg?react';
 
 export default function BoothLocation() {
   return (
@@ -6,7 +7,10 @@ export default function BoothLocation() {
       <S.Title>오시는 길</S.Title>
       <S.Locate>서울특별시 강남구 테헤란로 14길 6 남도빌딩 2층</S.Locate>
       <S.Map>
-        <S.Button>지도에서 보기</S.Button>
+        <S.Button>
+          <S.ButtonText>지도에서 보기</S.ButtonText>
+          <RightIcon width={'1rem'} height={'1rem'} fill="#FAFAFA" />
+        </S.Button>
       </S.Map>
     </S.Container>
   );
