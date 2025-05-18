a2642a6b6d617164d72f320cc69b5ff6e87f443f
javadocq <han18264@naver.com>
2025-05-19 02:20:19 +0900

    feat: img 추가 및 공지사항 데이터 입력

--- src/constants/main/Notice.ts
+++ src/constants/main/Notice.ts
@@ -4,79 +4,169 @@ import notice1_2 from '@/assets/images/notice/notice1-2.webp';
 import notice1_3 from '@/assets/images/notice/notice1-3.webp';
 import notice1_4 from '@/assets/images/notice/notice1-4.webp';
 import notice1_5 from '@/assets/images/notice/notice1-5.webp';
+import notice1_6 from '@/assets/images/notice/notice1-6.webp';
+import notice1_7 from '@/assets/images/notice/notice1-7.webp';
+import notice2_0 from '@/assets/images/notice/notice2-0.webp';
+import notice2_1 from '@/assets/images/notice/notice2-1.webp';
+import notice2_2 from '@/assets/images/notice/notice2-2.webp';
+import notice2_3 from '@/assets/images/notice/notice2-3.webp';
+import notice2_4 from '@/assets/images/notice/notice2-4.webp';
+import notice2_5 from '@/assets/images/notice/notice2-5.webp';
+import notice2_6 from '@/assets/images/notice/notice2-6.webp';
+import notice3_0 from '@/assets/images/notice/notice3-0.webp';
+import notice3_1 from '@/assets/images/notice/notice3-1.webp';
+import notice3_2 from '@/assets/images/notice/notice3-2.webp';
+import notice3_3 from '@/assets/images/notice/notice3-3.webp';
+import notice3_4 from '@/assets/images/notice/notice3-4.webp';
+import notice3_5 from '@/assets/images/notice/notice3-5.webp';
+import notice3_6 from '@/assets/images/notice/notice3-6.webp';
+import notice3_7 from '@/assets/images/notice/notice3-7.webp';
+import notice4_0 from '@/assets/images/notice/notice4-0.webp';
+import notice4_1 from '@/assets/images/notice/notice4-1.webp';
+import notice4_2 from '@/assets/images/notice/notice4-2.webp';
+import notice4_3 from '@/assets/images/notice/notice4-3.webp';
+import notice4_4 from '@/assets/images/notice/notice4-4.webp';
+import notice4_5 from '@/assets/images/notice/notice4-5.webp';
+import notice5_0 from '@/assets/images/notice/notice5-0.webp';
+import notice5_1 from '@/assets/images/notice/notice5-1.webp';
+import notice5_2 from '@/assets/images/notice/notice5-2.webp';
+import notice5_3 from '@/assets/images/notice/notice5-3.webp';
+import notice5_4 from '@/assets/images/notice/notice5-4.webp';
+import notice5_5 from '@/assets/images/notice/notice5-5.webp';
+import notice6_0 from '@/assets/images/notice/notice6-0.webp';
+import notice6_1 from '@/assets/images/notice/notice6-1.webp';
+import notice6_2 from '@/assets/images/notice/notice6-2.webp';
+import notice6_3 from '@/assets/images/notice/notice6-3.webp';
+import notice6_4 from '@/assets/images/notice/notice6-4.webp';
+import notice7_0 from '@/assets/images/notice/notice7-0.webp';
+import notice7_1 from '@/assets/images/notice/notice7-1.webp';
+import notice7_2 from '@/assets/images/notice/notice7-2.webp';
+import notice7_3 from '@/assets/images/notice/notice7-3.webp';
+import notice7_4 from '@/assets/images/notice/notice7-4.webp';
+import notice7_5 from '@/assets/images/notice/notice7-5.webp';
+import notice7_6 from '@/assets/images/notice/notice7-6.webp';
+import notice7_7 from '@/assets/images/notice/notice7-7.webp';
+import notice8_0 from '@/assets/images/notice/notice8-0.webp';
+import notice8_1 from '@/assets/images/notice/notice8-1.webp';
+import notice8_2 from '@/assets/images/notice/notice8-2.webp';
+import notice8_3 from '@/assets/images/notice/notice8-3.webp';
+import notice8_4 from '@/assets/images/notice/notice8-4.webp';
+import notice8_5 from '@/assets/images/notice/notice8-5.webp';
+import notice8_6 from '@/assets/images/notice/notice8-6.webp';
+import notice8_7 from '@/assets/images/notice/notice8-7.webp';
+import notice8_8 from '@/assets/images/notice/notice8-8.webp';
+import notice8_9 from '@/assets/images/notice/notice8-9.webp';
+import notice9_0 from '@/assets/images/notice/notice9-0.webp';
+import notice9_1 from '@/assets/images/notice/notice9-1.webp';
+import notice9_2 from '@/assets/images/notice/notice9-2.webp';
+import notice9_3 from '@/assets/images/notice/notice9-3.webp';
+import notice9_4 from '@/assets/images/notice/notice9-4.webp';
 
 export const NoticeData = [
   {
     id: 1,
-    img: [notice1_0, notice1_1, notice1_2, notice1_3, notice1_4, notice1_5],
-    title: '💥 2025 한양대학교 ERICA 봄 축제 ESPERO <썰을 낋여오너라~> 참가자 모집 공고 💥',
-    tags: [
-      { color: 'pk200', text: 'LIVE' },
-      { color: 'rd500', text: '공연무대' },
-    ],
-    body: `안녕하십니까, 2025 한양대학교 ERICA 봄 축제 ESPERO 축제기획단 공연무대팀 입니다. 🎭
+    img: [notice1_0, notice1_1, notice1_2, notice1_3, notice1_4, notice1_5, notice1_6, notice1_7],
+    title: '☘️2025 한양대학교 ERICA 봄축제 굿즈 사전구매 공고☘️',
+    tags: [{ color: 'pk200', text: '플리마켓' }],
+    body: `☘️2025 한양대학교 ERICA 봄축제 굿즈 사전구매 공고☘️
 
-많은 관객에게 자신의 이야기를 나눌 수 있는 ⭐️깜짝이벤트⭐️를 준비했습니다. 재밌는 이야기🤣, 흑역사🙈, 사랑 이야기😍 등 다양한 썰을 공유하고 상품도 받아 가세요!
+안녕하십니까, 2025 한양대학교 ERICA 봄 축제 ESPERO:HYLIGHT 축제기획단 플리마켓팀입니다. 봄 내음 가득한 봄축제 굿즈 사전구매를 진행합니다! 따뜻한 봄과 함께할 특별한 아이템을 미리 만나보세요.💚
 
-📝 참여 기간: 2025.05.05(월) 10:00 ~ 2025.05.16(금) 23:59
+✅사전구매 기간: 2025.04.05(토) ~ 2025.04.09(수)✅신청 대상: 한양대학교 ERICA 재학생, 휴학생, 졸업생, 대학원생 및 외부인✅구매 방법: 총학생회 인스타 링크트리 내 구글폼으로 신청링크 주소: https://forms.gle/aDcFaxS2DMW5MHF68
 
-✅ 참여 자격: 한양대학교 ERICA 재학생 또는 휴학생 개인
-✅ 신청 방법
+자세한 사항은 ’카드뉴스‘ 및 링크트리 내 ’구글폼‘을 통해 확인해 주시길 바랍니다☺️💚
 
-1. 총학생회 NOTE 인스타그램 프로필의 봄축제 링크 트리 클릭
-2. 링크 트리에서 ‘봄 축제 무대 이벤트 참여 구글 폼‘ 작성 후 제출 <구글폼 주소: [https://forms.gle/7kmcMPva31Gwwos79](https://forms.gle/7kmcMPva31Gwwos79)>
+❕본 조사는 사전 주문으로 진행되며 입금 후 환불이 불가합니다.
+❕사전구매 기간에는 등번호 선택이 가능합니다.
+❕현장판매 기간에는 등번호 선택이 불가합니다.
 
-📝 상품 증정 방법: 무대 위에서 추첨을 통해 선정된 썰의 주인공 중 최대 ’세 분‘께 상품 증정
+문의사항: ESPERO_플리마켓팀 카카오톡 채널
 
-❗️익명으로 참여하신 분들은 상품 증정이 어렵습니다.
-
-* 익명 혹은 실명을 선택하여 참여할 수 있으며, 익명으로 참여한 경우에는 상품 수령이 불가합니다.
-* 공유하기 부적합한 내용이라고 판단되는 경우, 추첨에서 배제될 수 있습니다.
-* 접수된 내용에 관한 모든 책임은 오로지 신청자 본인에게 있습니다.
--*축제 기간 중 이벤트 진행을 하지 않았거나 선정된 사람이 없을 경우, 상품 지급은 취소됩니다.
-
-- 자세한 사항 및 유의사항은 카드뉴스를 확인해 주세요.
-- 추가 문의 사항은 [2025 ESPERO 공연무대팀] 카카오톡 채널로 문의 바랍니다.`,
+#봄축제기획단 #2025ESPERO플리마켓팀`,
   },
   {
     id: 2,
-    img: [notice1_0, notice1_1, notice1_2, notice1_3, notice1_4, notice1_5],
-    title: '안전 수칙 준수 안내',
-    tags: [
-      { color: 'pk200', text: 'LIVE' },
-      { color: 'rd500', text: '공연무대' },
-    ],
-    body: '축제 기간 동안 안전 사고 예방을 위해 반드시 안전 수칙을 준수해 주시기 바랍니다.',
+    img: [notice2_0, notice2_1, notice2_2, notice2_3, notice2_4, notice2_5, notice2_6],
+    title: '🎙️ 2025 한양대학교 ERICA 봄 축제 복면가왕 ‘히든스타’ 참가자 모집공고 🎙️',
+    tags: [{ color: 'rd500', text: '공연무대' }],
+    body: `🎙️ 2025 한양대학교 ERICA 봄 축제 ESPERO 복면가왕 ‘히든스타’ 참가자 모집공고 🎙️
+
+안녕하십니까, 2025 한양대학교 ERICA 봄 축제 ESPERO 축제기획단 공연무대팀입니다. 🎭
+
+교내에 숨어있던 실력자들이 무대 위에서 ⭐️HYDDEN STAR⭐️로 빛나는 시간!
+축제 당일 무대에서 히든스타의 주인공이 될 여러분을 기다리고 있습니다.
+
+📝 예선 신청 일시: 2025.04.07(월) 10:00 ~ 2025.04.21(월) 23:59
+
+📝 예선 일자 및 장소: 2025.04.30(수) 컨퍼런스 홀 4층, 중강당
+
+✅ 예선 신청 방법1. 총학생회 비상대책위원회 인스타그램 프로필의 링크트리 클릭
+2. 링크트리 내 ’히든스타 참가자 모집공고‘ 구글폼 작성https://docs.google.com/forms/d/e/1FAIpQLSeQuJpJfHAKN5lED1-UuQFkoMzLEpV5HypcdBeOJkAKXwv6HA/viewform
+
+📝 본선 참가자 발표: 2025.05.02(금) 개별 연락
+
+* 개인 단위로만 지원 가능합니다.
+* 예선에서는 가사를 보고 부르는 것이 가능하지만, 축제 당일 무대에서는 가사를 외워서 불러주셔야 합니다.
+* 개인 악기는 지참이 불가합니다.
+
+- 자세한 사항 및 유의사항은 카드뉴스를 확인해 주세요.
+- 프로필의 ’링크트리‘ 내에 있는 ‘히든스타 참가자 모집공고’ 구글폼에서 더 자세한 내용을 확인할 수 있습니다.`,
   },
   {
     id: 3,
-    img: [notice1_0, notice1_1, notice1_2, notice1_3, notice1_4, notice1_5],
-    title: '이벤트 참여 안내',
-    tags: [
-      { color: 'pk200', text: 'LIVE' },
-      { color: 'rd500', text: '공연무대' },
-    ],
-    body: '축제 기간 동안 진행되는 다양한 이벤트에 참여해 보세요! 자세한 내용은 홈페이지를 참고하세요.',
+    img: [notice3_0, notice3_1, notice3_2, notice3_3, notice3_4, notice3_5, notice3_6, notice3_7],
+    title: '☘️굿즈 사전구매 기간 연장 안내☘️',
+    tags: [{ color: 'pk200', text: '플리마켓' }],
+    body: `️!!2025 한양대학교 ERICA 봄축제 굿즈 사전구매 기간연장!!☘️
+
+안녕하십니까, 2025 한양대학교 ERICA 봄 축제 ESPERO:HYLIGHT 축제기획단 플리마켓팀 ”청춘팔어람“입니다. 봄축제 굿즈 사전구매 기간을 연장하게 되었습니다! 마지막 구매 일정이니 관심있으신 분들은 빠르게 구매해주세요💚
+
+✅사전구매 기간: 2025.04.10(목) ~ 2025.04.11(금)
+✅신청 대상: 한양대학교 ERICA 재학생, 휴학생, 졸업생, 대학원생 및 외부인
+✅구매 방법: 총학생회 인스타 링크트리 내 구글폼으로 신청링크 주소: https://forms.gle/aDcFaxS2DMW5MHF68
+
+자세한 사항은 ’카드뉴스‘ 및 링크트리 내 ’구글폼‘을 통해 확인해 주시길 바랍니다☺️💚
+
+❕본 조사는 사전 주문으로 진행되며 입금 후 환불이 불가합니다.
+❕사전구매 기간에는 등번호 선택이 가능합니다.
+❕현장판매 기간에는 등번호 선택이 불가합니다.
+
+문의사항: ESPERO_플리마켓팀 카카오톡 채널
+
+#봄축제기획단 #2025ESPERO플리마켓팀`,
   },
   {
     id: 4,
-    img: [notice1_0, notice1_1, notice1_2, notice1_3, notice1_4, notice1_5],
-    title: '주차장 이용 제한 안내',
-    tags: [
-      { color: 'pk200', text: 'LIVE' },
-      { color: 'rd500', text: '공연무대' },
-    ],
-    body: '대중교통을 이용해 주세요.',
+    img: [notice4_0, notice4_1, notice4_2, notice4_3, notice4_4, notice4_5],
+    title: '💚 플리마켓 외부 셀러 모집 공고 💚',
+    tags: [{ color: 'pk200', text: '플리마켓' }],
+    body: `💚2025 한양대학교 ERICA 봄 축제 ESPERO:HYLIGHT 플리마켓 ‘청춘팔어람’ 외부셀러 모집💚
+
+안녕하십니까, 2025 한양대학교 ERICA 봄축제 ESPERO:HYLIGHT 플리마켓팀 ‘청춘팔어람‘ 입니다.
+
+봄 축제 3일간 플리마켓에서 축제를 함께 가꾸어 나갈 일반부스 외부셀러를 모집합니다.
+
+📍신청방법 : 링크트리 구글폼 제출
+📍링크 주소 : https://forms.gle/FLgr2KaFXVLQhuQP9
+💥셀러 모집 기간 : 2025.04.14 (월) ~ 2025.04.25 (금)
+💥셀러 신청 대상 : HYLIGHT를 더 풍요롭게 만들어 줄 열정을 가진 분들 누구나! (부스 대표자는 성인이어야 함)
+💥셀러 확정 발표 : 2025.04.30(수)
+💥부스 운영 기간 : 2025.05.27(화) ~ 2025.05.29(목) 11:00 - 17:00
+
+자세한 사항은 카드뉴스를 참고해주세요.
+
+문의사항: ESPERO_플리마켓팀 카카오톡 채널
+
+#봄축제기획단 #2025ESPERO플리마켓팀`,
   },
   {
     id: 5,
-    img: [notice1_0, notice1_1, notice1_2, notice1_3, notice1_4, notice1_5],
-    title: '💥 2025 한양대학교 ERICA 봄 축제 ESPERO <썰을 낋여오너라~> 참가자 모집 공고 💥',
-    tags: [
-      { color: 'pk200', text: 'LIVE' },
-      { color: 'rd500', text: '공연무대' },
-    ],
-    body: `안녕하십니까, 2025 한양대학교 ERICA 봄 축제 ESPERO 축제기획단 공연무대팀 입니다. 🎭
+    img: [notice5_0, notice5_1, notice5_2, notice5_3, notice5_4, notice5_5],
+    title: '💥<썰을 낋여오너라~> 참가자 모집 공고 💥',
+    tags: [{ color: 'rd500', text: '공연무대' }],
+    body: `💥 2025 한양대학교 ERICA 봄 축제 ESPERO <썰을 낋여오너라~> 참가자 모집 공고 💥
+
+안녕하십니까, 2025 한양대학교 ERICA 봄 축제 ESPERO 축제기획단 공연무대팀 입니다. 🎭
 
 많은 관객에게 자신의 이야기를 나눌 수 있는 ⭐️깜짝이벤트⭐️를 준비했습니다. 재밌는 이야기🤣, 흑역사🙈, 사랑 이야기😍 등 다양한 썰을 공유하고 상품도 받아 가세요!
 
@@ -86,7 +176,7 @@ export const NoticeData = [
 ✅ 신청 방법
 
 1. 총학생회 NOTE 인스타그램 프로필의 봄축제 링크 트리 클릭
-2. 링크 트리에서 ‘봄 축제 무대 이벤트 참여 구글 폼‘ 작성 후 제출 <구글폼 주소: [https://forms.gle/7kmcMPva31Gwwos79](https://forms.gle/7kmcMPva31Gwwos79)>
+2. 링크 트리에서 ‘봄 축제 무대 이벤트 참여 구글 폼‘ 작성 후 제출 <구글폼 주소: https://forms.gle/7kmcMPva31Gwwos79>
 
 📝 상품 증정 방법: 무대 위에서 추첨을 통해 선정된 썰의 주인공 중 최대 ’세 분‘께 상품 증정
 
@@ -94,20 +184,135 @@ export const NoticeData = [
 
 * 익명 혹은 실명을 선택하여 참여할 수 있으며, 익명으로 참여한 경우에는 상품 수령이 불가합니다.
 * 공유하기 부적합한 내용이라고 판단되는 경우, 추첨에서 배제될 수 있습니다.
-* 접수된 내용에 관한 모든 책임은 오로지 신청자 본인에게 있습니다.
--*축제 기간 중 이벤트 진행을 하지 않았거나 선정된 사람이 없을 경우, 상품 지급은 취소됩니다.
+* 접수된 내용에 관한 모든 책임은 오로지 신청자 본인에게 있습니다.-
+*축제 기간 중 이벤트 진행을 하지 않았거나 선정된 사람이 없을 경우, 상품 지급은 취소됩니다.
 
 - 자세한 사항 및 유의사항은 카드뉴스를 확인해 주세요.
-- 추가 문의 사항은 [2025 ESPERO 공연무대팀] 카카오톡 채널로 문의 바랍니다.`,
+- 추가 문의 사항은 [2025 ESPERO 공연무대팀] 카카오톡 채널로 문의 바랍니다.
+
+#봄축제기획단 #2025ESPERO공연무대팀`,
   },
   {
     id: 6,
-    img: [notice1_0, notice1_1, notice1_2, notice1_3, notice1_4, notice1_5],
-    title: '안전 수칙 준수 안내',
-    tags: [
-      { color: 'pk200', text: 'LIVE' },
-      { color: 'rd500', text: '공연무대' },
+    img: [notice6_0, notice6_1, notice6_2, notice6_3, notice6_4],
+    title: '☘️굿즈 사전구매 수령 안내☘️',
+    tags: [{ color: 'pk200', text: '플리마켓' }],
+    body: `☘️2025 한양대학교 ERICA 봄 축제 ESPERO:HYLIGHT 플리마켓 ‘청춘팔어람’ 굿즈 사전구매 수령 공지☘️
+
+안녕하십니까, 2025 한양대학교 ERICA 봄축제 ESPERO:HYLIGHT 축제 기획단 플리마켓팀 ‘청춘팔어람‘ 입니다.
+
+다양한 즐거움과 추억을 가져다주는 봄 축제를 위해 ERICA 학생 여러분들의 사전구매하신 굿즈 수령에 관해 공지합니다💚
+
+✅ 수령 기간: 2025.05.27(화) ~ 2025.05.29(목) 3일간
+✅ 수령 위치: 학생 복지관 앞 민주광장 플리마켓 운영본부
+✅ 수령 시간: 11:00 ~ 16:00 (해당 시간에만 수령 가능합니다.)
+
+❗️본인 확인을 위해 신분증 혹은 학생증을 필히 지참하셔야 합니다. (모바일 신분증 및 학생증 가능)
+❗️수령 후 현장에서 불량 및 누락 확인하고, 이후 반품 및 환불은 불가합니다.
+
+자세한 사항은 카드뉴스를 통해 확인해 주시길 바랍니다☺️💚
+
+문의사항 : ESPERO_플리마켓팀 카카오톡 채널
+
+#봄축제기획단 #2025ESPERO플리마켓팀`,
+  },
+  {
+    id: 7,
+    img: [notice7_0, notice7_1, notice7_2, notice7_3, notice7_4, notice7_5, notice7_6, notice7_7],
+    title: '❤️미니 소개팅 모집 공고',
+    tags: [{ color: 'rd500', text: '콘텐츠' }],
+    body: `2025 한양대학교 ERICA 봄축제 ”ESPERO:HYLIGHT“ 미니 소개팅 모집 공고
+
+✨NOW OR NEVER, HYLIGHT✨
+
+안녕하세요! 2025 봄축제기획단 콘텐츠 팀입니다!
+
+같은 맛 사탕을 고른 운명의 상대와 함께 축제 이곳저곳을 돌아다니며 사랑을 키워나가는 ❤️사랑처럼 달콤하다는데? 미니 소개팅❤️을 소개합니다!
+
+낭만과 청춘으로 가득 차 설레는 축제 분위기에 맞춰, 청춘을 더 빛나게 만들어줄 새로운 인연을 만나보는 건 어떨까요?
+
+🕛 행사 일시: 2025년 5월 27일 (화) ~ 2025년 5월 29일 (목) 11:00 / 13:00 / 15:00 하루에 총 3타임 진행
+🚩행사 장소: 호수공원 신전
+
+모집 안내
+✔️신청 기간: 2025년 5월 12일 (월) ~ 2025년 5월 15일 (목) 23:59
+✔️신청 방법: 총학생회 NOTE 인스타그램 축제기획단 링크트리 내 구글폼을 통해 신청서 작성
+✔️ 링크 주소: https://forms.gle/6bfLzc42UR66wekT9
+
+참여자분들에게 추후 확정 안내 문자를 드릴 예정입니다.
+
+자세한 내용은 카드뉴스를 참고해주세요 📩✨
+
+📌문의사항
+2025 ESPERO 콘텐츠팀 카카오 채널
+콘텐츠 팀장 이다성 010-3127-3462`,
+  },
+  {
+    id: 8,
+    img: [
+      notice8_0,
+      notice8_1,
+      notice8_2,
+      notice8_3,
+      notice8_4,
+      notice8_5,
+      notice8_6,
+      notice8_7,
+      notice8_8,
+      notice8_9,
     ],
-    body: '축제 기간 동안 안전 사고 예방을 위해 반드시 안전 수칙을 준수해 주시기 바랍니다.',
+    title: '🌊호공 레이스 참가자 모집 공고',
+    tags: [{ color: 'rd500', text: '콘텐츠' }],
+    body: `2025 한양대학교 ERICA 봄축제 ”ESPERO:HYLIGHT“
+
+✨NOW OR NEVER, HYLIGHT✨
+
+안녕하세요! 2025 봄축제기획단 콘텐츠팀입니다!
+
+직접 만든 DIY 배를 타고 호수공원을 가로지르며 속도를 겨루는 🌊호공레이스🌊를 소개합니다
+
+시험 공부? 취준? 무슨 소리 하는거야, 너 꿈 꿨어?? 우리 호수공원 건너고 있었잖아...
+
+1~4인이 한 팀을 이루어 배를 제작하고, 팀원 중 한 명이 직접 배에 올라 경기에 참가합니다. 아이디어와 열정, 그리고 젖을 각오만 있다면 호수 위의 대결에 도전해보세요!
+
+☑️ 행사 일시: 2025년 5월 28일 (수) 13~15시
+☑️ 행사 장소: 한양대학교 ERICA 호수공원
+
+참가 신청 안내
+📌신청 기간: 2025년 5월 12일 ~ 5월 20일
+📌신청 방법: 총학생회 NOTE 인스타그램 축제기획단 링크트리 내 구글폼을 통해 신청서 작성
+📌신청 링크: https://forms.gle/Kdv7GzthpKBvYHDL9
+
+구글폼 작성자분들에게 추후 안내 문자를 드릴 예정입니다. 공지된 유의사항 및 안전수칙을 숙지하지 않아 발생하는 실격 등 불이익에 대한 책임은 참가자 본인에게 있습니다
+
+자세한 내용은 카드뉴스를 참고해주세요 🌟
+
+📌문의사항
+2025 ESPERO 콘텐츠팀 카카오톡채널
+콘텐츠 팀장 이다성 010-3127-3462`,
+  },
+  {
+    id: 9,
+    img: [notice9_0, notice9_1, notice9_2, notice9_3, notice9_4],
+    title: '✨LIGHT GUARD 규찰대 소개',
+    tags: [],
+    body: `2025 한양대학교 ERICA 봄축제 ESPERO“ESPERO:HYLIGHT”
+
+✨ NOW OR NEVER, HYLIGHT ✨
+
+✨2025 봄축제 ESPERO:HYLIGHT 즐길 준비 되셨나요?✨
+청춘의 가장 눈부신 순간이 될 HYLIGHT를 수호하는 LIGHT GUARD를 소개합니다
+
+💫LIGHT GUARD란?💫
+학우 여러분들이 신나는 축제를 보다 안전하게 즐기실 수 있도록 학교 곳곳을 관리 및 순찰하는 규찰대입니다!
+
+💫LIGHT GUARD 근무 일정💫2025.05.27(화) ~ 2025.05.29(목) 총 3일PM 17:00 ~ AM 01:00
+
+🚨축제 기간 학우 여러분들께서는 즐겁고 안전한 축제를 위해 규찰대 지시에 협조해 주실 것을 부탁드립니다.🚨
+
+보다 자세한 내용은 카드뉴스를 참고 바랍니다.
+
+문의사항
+규찰대 카카오톡 - http://pf.kakao.com/_JxbxnPn`,
   },
 ];
