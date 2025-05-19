8f0c9ff81f0ddd30ba5c2aa936778b8b6e28432e
ksundev <kshk08@hanyang.ac.kr>
2025-05-19 21:12:41 +0900

    feat: 카테고리 목록 업데이트 및 불필요한 항목 제거

--- src/constants/map/CATEGORIES.ts
+++ src/constants/map/CATEGORIES.ts
@@ -3,13 +3,12 @@ const categories = [
   '주류 구매 위치',
   '플리마켓',
   '프로모션',
+  '콘텐츠',
   '화장실',
   '공연장',
   '셔틀콕',
   '푸트트럭',
-  '흡연실',
-  '의무실',
-  '기타',
+  '흡연구역',
 ] as const;
 type CATEGORIES = (typeof categories)[number];
 
