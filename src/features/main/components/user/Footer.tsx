02e27c29725f517307fb15fc2e33dfabdaf5fdc4
SeHyun <rla6170@naver.com>
2025-05-22 01:57:28 +0900

    feat: 로그아웃 로그인 토글 추가

--- src/features/main/components/user/Footer.tsx
+++ src/features/main/components/user/Footer.tsx
@@ -1,9 +1,19 @@
 import { useNavigate } from 'react-router-dom';
 import * as S from './Footer.styles';
 import Instagram from '@/assets/icons/instagram.svg?react';
+import { useAuthStore } from '@/stores/useAuthStore';
+import { logout } from '@/services/login';
 export default function Footer() {
   const navigate = useNavigate();
-
+  const isLoggined = useAuthStore((state) => state.isLoggedIn);
+  const handleLoginClick = () => {
+    if (isLoggined) {
+      logout();
+      window.location.reload();
+    } else {
+      navigate('/login');
+    }
+  };
   return (
     <S.Container>
       <Instagram width={'2.5rem'} height={'2.5rem'} />
@@ -16,7 +26,7 @@ export default function Footer() {
         @LIKELION ERICA. All Rights Reserved.
       </S.Sub>
       <S.Btn>
-        <S.Out onClick={() => navigate('/login')}>로그인하기</S.Out>
+        <S.Out onClick={handleLoginClick}>{isLoggined ? '로그아웃하기' : '로그인하기'}</S.Out>
       </S.Btn>
     </S.Container>
   );
