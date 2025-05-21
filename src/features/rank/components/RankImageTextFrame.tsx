a022088cfb24157edf9ab44df75c48575745a258
SeHyun <rla6170@naver.com>
2025-05-21 16:54:05 +0900

    feat: 등락 표현 image 추가, database 연결

--- src/features/rank/components/RankImageTextFrame.tsx
+++ src/features/rank/components/RankImageTextFrame.tsx
@@ -2,25 +2,48 @@ import { ImageTextFrameWithOrganization } from '@/components/image-text-frame';
 import * as S from './RankImageTextFrame.styles';
 import { LikeButton } from '@/features/like';
 import { BOOTH_LIST } from '@/constants/booth/booth';
+import ArrowIcon from '@/assets/icons/nrk_arrow-dropdown.svg?react';
+import { useNavigate } from 'react-router-dom';
 
 export default function RankImageTextFrame({
   id,
   likeCount,
   index,
+  prevRank,
+  currentRank,
 }: {
   id: number;
   likeCount: number;
   index: number;
+  prevRank?: number;
+  currentRank?: number;
 }) {
   const booth = BOOTH_LIST.find((booth) => booth.id === id);
+  const navigate = useNavigate();
   if (!booth) {
     return null; // or handle the case when the booth is not found
   }
+  const diff = prevRank !== undefined && currentRank !== undefined ? prevRank - currentRank : 0;
   return (
     <S.Container $first={index === 1}>
       <S.Wrapper>
         <S.Rank>
           <S.RankNumber>{index}</S.RankNumber>
+          {diff === 0 ? (
+            <S.RankFrame>
+              <S.DrawText>-</S.DrawText>
+            </S.RankFrame>
+          ) : (
+            <S.RankFrame>
+              <ArrowIcon
+                width="0.75rem"
+                height="0.75rem"
+                fill={diff > 0 ? '#45E02A' : '#F55353'}
+                style={diff > 0 ? { rotate: '0deg' } : { rotate: '180deg' }}
+              />
+              {diff > 0 ? <S.UpText>{diff}</S.UpText> : <S.DownText>{Math.abs(diff)}</S.DownText>}
+            </S.RankFrame>
+          )}
         </S.Rank>
         <ImageTextFrameWithOrganization
           image={booth.profileImage}
@@ -28,8 +51,11 @@ export default function RankImageTextFrame({
           organization={booth.affiliation}
           canPickup={false}
           width="100%"
+          onClick={() => {
+            navigate(`/booth/${booth.id}`);
+          }}
         />
-        <LikeButton id={id} like={Number(likeCount)} />
+        <LikeButton id={id} like={Number(likeCount)} key={id + likeCount} />
       </S.Wrapper>
     </S.Container>
   );
