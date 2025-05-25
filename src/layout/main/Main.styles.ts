9a4ffcdf2563fab1296332ab8e4ef2c8b8e740d6
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-05-25 15:08:02 +0900

    feat: x scroll 제한한

--- src/layout/main/Main.styles.ts
+++ src/layout/main/Main.styles.ts
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 export const Main = styled(motion.main)<{ $isMapPage: boolean }>`
   flex: 1;
-  overflow-y: auto;
+  overflow: hidden auto;
   z-index: 0;
   padding-top: ${(props) => (props.$isMapPage ? '0' : '3.875rem')};
 `;
